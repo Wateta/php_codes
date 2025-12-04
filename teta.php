e18f878df134ae3f22d1977a8542cfd943fee1a9
Wateta <shimirwasonia5@gmail.com>
2025-12-04 13:48:58 +0200

    Add files via upload

---
+++ teta.php
@@ -0,0 +1,33 @@
+<?php
+/*$x=5;
+ECHO 
+//var_dump is used to show the data type of a value
+var_dump($x);
+$a="Hlo sonia!";
+echo strtoupper($a);
+//replacing some characters in a string
+$x="Hello world!";
+echo str_replace("World","Dolly",$x);
+// strrev()reservers a string
+$z="Hello sonia";
+echo strrev($z);
+//removing a white space using trim()
+echo trim($z);
+//explode() splits a string into an array
+$y=explode(" ",$x);
+print_r($y);
+//concatinating a string we use dot(.)
+echo $x.$a;
+//extracting some part of a string
+$x="Hello world";
+echo substr($x,6,5);
+//extracting a string to the end
+echo substr($x,6);
+//starting from the end of indexes
+echo substr($x,-5,-3);
+echo "I want to be a \"software and cybersecurity engineer\" "
+is_int()used to check if a type of a variable is an integer*/
+ //checking if a variable is numeric
+ $x="hello";
+ var_dump(is_numeric($x));
+?>
